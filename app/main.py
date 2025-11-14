94256dc6d2c055ce3714303c7bc3fd04420cbbb7
monsco <muhammadvzarza1905@gmail.com>
2025-11-14 21:37:50 +0700

    inisiasi dokumentasi api

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import ORJSONResponse, RedirectResponse
 
 
 app = FastAPI(default_response_class=ORJSONResponse)
 
+app.mount(
+    r'/static',
+    StaticFiles(directory=r'app/static'),
+    name='static',
+)
 
-@app.get('/')
+
+@app.get('/', response_class=RedirectResponse)
 async def index():
-    return {'message': 'go to http://localhost:8080/docs to see the documentation'}
+    return r'/static/index.html'
+
+
+@app.get('/ping')
+async def ping_pong():
+    return {'message': 'pong'}
+
 
 def main():
     from fastapi_cli import cli
@@ -15,6 +28,7 @@ def main():
 
     os.system('cls' if os.name == 'nt' else 'clear')
     cli.dev()
+
 
 if __name__ == '__main__':
     main()
